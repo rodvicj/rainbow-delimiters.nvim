fba5c38e0ade58155f6c4385b5801be04b5ea9b7
rodvicj <rodvicj@gmail.com>
2024-12-10 19:59:10 +0800

    astro simplify color

--- queries/astro/rainbow-delimiters.scm
+++ queries/astro/rainbow-delimiters.scm
@@ -1,12 +1,12 @@
-(element
-  (start_tag
-    "<" @delimiter
-    (tag_name) @delimiter
-    ">" @delimiter)
-  (end_tag
-    "</" @delimiter
-    (tag_name) @delimiter
-    ">" @delimiter @sentinel)) @container
+; (element
+;   (start_tag
+;     "<" @delimiter
+;     (tag_name) @delimiter
+;     ">" @delimiter)
+;   (end_tag
+;     "</" @delimiter
+;     (tag_name) @delimiter
+;     ">" @delimiter @sentinel)) @container
 
 (html_interpolation
   "{" @delimiter
