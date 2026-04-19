3d7cdecbe078f5d97182dbdde65b91186e09c05e
rodvicj <32471816+rodvicj@users.noreply.github.com>
2026-04-19 11:51:46 +0800

    Remove self-closing tag and style element definitions
    Removed self-closing tag and style element definitions from rainbow-delimiters.scm.

--- queries/astro/rainbow-delimiters.scm
+++ queries/astro/rainbow-delimiters.scm
@@ -1,19 +1,3 @@
-(element
-  (self_closing_tag
-    "<" @delimiter
-    (tag_name) @delimiter
-    "/>" @delimiter)) @container
-
-(style_element
-  (start_tag
-    "<" @delimiter
-    (tag_name) @delimiter
-    ">" @delimiter)
-  (end_tag
-    "</" @delimiter
-    (tag_name) @delimiter
-    ">" @delimiter)) @container
-
 (html_interpolation
   "{" @delimiter
   "}" @delimiter @sentinel) @container
